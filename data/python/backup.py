61b14e733ec7a135722e59dab7a166e736f7919d
guandasheng <60736835+guandasheng@users.noreply.github.com>
2023-12-31 12:25:34 +0800

    Create backup.py

---
+++ data/python/backup.py
@@ -0,0 +1,25 @@
+import os  
+import shutil  
+from datetime import datetime  
+  
+def backup_rules():  
+    # 获取当前日期  
+    current_date = datetime.now().strftime('%Y-%m-%d')  
+      
+    # 定义源文件和目标文件夹  
+    src_file = 'rules.txt'  
+    dst_folder = '历史版本'  
+      
+    # 创建目标文件夹（如果不存在）  
+    if not os.path.exists(dst_folder):  
+        os.makedirs(dst_folder)  
+      
+    # 获取目标文件的完整路径  
+    dst_file = os.path.join(dst_folder, current_date + '_' + os.path.basename(src_file))  
+      
+    # 复制文件并重命名  
+    shutil.copy2(src_file, dst_file)  
+    print(f"备份完成: {src_file} -> {dst_file}")  
+  
+if __name__ == '__main__':  
+    backup_rules()
