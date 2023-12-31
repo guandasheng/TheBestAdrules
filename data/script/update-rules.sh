6da63070087911bf65f92e9dc68cfd6f79904f98
guandasheng <60736835+guandasheng@users.noreply.github.com>
2024-01-01 00:41:49 +0800

    Update update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -160,4 +160,17 @@ wait
 echo '更新成功'
 rm -rf pre
 
+# 检查 history 目录是否存在  
+if [ ! -d "./history" ]; then  
+  # 如果目录不存在，则创建它  
+  mkdir -p ./history  
+fi  
+  
+# 复制 rules.txt 到 history 目录  
+cp ./rules.txt ./history/  
+  
+# 压缩 rules.txt 为 zip 格式，并使用当前时间作为新文件名  
+zip -r ./history/$(date +'%Y%m%d%H%M%S').zip ./rules.txt
+
+
 exit
