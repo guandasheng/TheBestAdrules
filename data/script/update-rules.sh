fbd7c6287524cae9d4793d45c64cf996a4fe32b3
guandasheng <60736835+guandasheng@users.noreply.github.com>
2024-01-01 11:27:54 +0800

    Update update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -34,6 +34,8 @@ rules=(
   "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt"
   "https://raw.githubusercontent.com/lingeringsound/10007_auto/master/reward"
   "https://raw.githubusercontent.com/hululu1068/AdGuard-Rule/main/rule/adgh.txt"
+  "https://raw.githubusercontent.com/DoingDog/XXKiller/main/f.txt"
+  
  )
 
 allow=(
