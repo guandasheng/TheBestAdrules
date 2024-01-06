08dca385afff13fd629428bea86944ed37d8798b
guandasheng <60736835+guandasheng@users.noreply.github.com>
2024-01-06 17:17:23 +0800

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -9,6 +9,6 @@ new_lines=$((total_lines - 8))
 
 # 获取当前时间（格式化为中文）
 current_time=$(date +"规则更新时间：%Y年%m月%d日 %H时%M分%S秒")
-
+sed -i '7d' all.txt
 # 在第7行后插入当前时间和行数信息
 sed -i "7a\\$current_time 更新共 $new_lines 规则" all.txt
